1e546f9ed4e73a6e85e3e3e9be3e0b27a30d2f5d
Nino Chimento <saninoc84@gmail.com>
2020-02-25 12:59:24 +0100

    6 cd

--- database.php
+++ database.php
@@ -24,5 +24,17 @@ $raccolta = [
     'year' => 1968,
     'poster' => 'https://img.discogs.com/En-I1oGmNh6t9491tfCOsxzsEy4=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4141703-1492449982-6548.jpeg.jpg'
   ],
+  [
+    'title' => 'America Idiot',
+    'author' => 'Greenday',
+    'year' => 2006,
+    'poster' => 'https://img.discogs.com/AXZRAwcNYdqqbhPeUWChPx4e4Tw=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-973706-1488163916-8517.jpeg.jpg'
+  ],
+  [
+    'title' => 'Nord SUd Ovest Est',
+    'author' => '883',
+    'year' => 2000,
+    'poster' => 'https://img.discogs.com/qPBLPTcPHHt5zgT6LU58jc9XOJg=/fit-in/600x592/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1209145-1266073228.jpeg.jpg'
+  ],
 ]
  ?>
