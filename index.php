b0a4bcbfe44987d9a5095c4285adb22657c84169
Nino Chimento <saninoc84@gmail.com>
2020-02-25 12:12:23 +0100

    start

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+
+    <title></title>
+  </head>
+  <body>
+    <div class="warap-cd">
+      <div class="cd">
+        <img src="" alt="">
+        <h2></h2>
+        <h4></h4>
+        <h6></h6>
+      </div>
+    </div>
+    <script src="dist/app.js" charset="utf-8"></script>
+  </body>
+</html>
